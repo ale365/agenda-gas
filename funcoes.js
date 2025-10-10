2fd1706995543bccf1d4190cd91955844e43c330
Alexandre365 <alexandre.de.paul@gmail.com>
2025-10-10 00:41:09 -0300

    puxando dados da tabela para app web

--- funcoes.js
+++ funcoes.js
@@ -8,3 +8,11 @@ function obterDadosHtml(nome)
 {
     return HtmlService.createHtmlOutputFromFile(nome).getContent();
 }
+
+
+function obterContatos()
+{
+    let folha = SpreadsheetApp.openById('1sHpJLRY47dqmYNVvVgLDdDeree61VXQ3nRw2usVa1TI').getActiveSheet();
+    let dados = folha.getDataRange().getValues();
+    return dados;
+}
